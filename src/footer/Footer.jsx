4df8073a34d512a2c2dfb9b5a4decc640d147209
Muhammad Ali <ali.aa251104@gmail.com>
2022-02-11 04:30:19 +0500

    Footer(commit on 'master')

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -11,28 +11,28 @@ const Footer = () => {
                     <div class="col-sm-4 col-md-3 item">
                         <h3>Services</h3>
                         <ul>
-                            <li><a href="">Delicious Flavours</a></li>
-                            <li><a href="">Natural Flavours</a></li>
-                            <li><a href="">Customer Staisfaction</a></li>
+                            <li><a href="JavaScript:void(0)">Delicious Flavours</a></li>
+                            <li><a href="JavaScript:void(0)">Natural Flavours</a></li>
+                            <li><a href="JavaScript:void(0)">Customer Staisfaction</a></li>
                         </ul>
                     </div>
                     <div class="col-sm-4 col-md-3 item">
                         <h3>About</h3>
                         <ul>
-                            <li><a href="">Company</a></li>
-                            <li><a href="">Team</a></li>
-                            <li><a href="">Legacy</a></li>
+                            <li><a href="JavaScript:void(0)">Company</a></li>
+                            <li><a href="JavaScript:void(0)">Team</a></li>
+                            <li><a href="JavaScript:void(0)">Legacy</a></li>
                         </ul>
                     </div>
                     <div class="col-sm-4 col-md-3 item">
                         <h3>Careers</h3>
                         <ul>
-                            <li><a href="">Job openings</a></li>
-                            <li><a href="">Employee success</a></li>
-                            <li><a href="">Benefits</a></li>
+                            <li><a href="JavaScript:void(0)">Job openings</a></li>
+                            <li><a href="JavaScript:void(0)">Employee success</a></li>
+                            <li><a href="JavaScript:void(0)">Benefits</a></li>
                         </ul>
                     </div>
-                    <div class="col-lg-3 item social"><a href=""><FaFacebookF /></a><a href=""><FaInstagram /></a><a href=""><FaTwitter /></a><a href=""><FaGithub /></a>
+                    <div class="col-lg-3 item social"><a href="JavaScript:void(0)"><FaFacebookF /></a><a href="JavaScript:void(0)"><FaInstagram /></a><a href="JavaScript:void(0)"><FaTwitter /></a><a href="JavaScript:void(0)"><FaGithub /></a>
                         <p class="copyright">Royal Scoops © 2022</p>
                     </div>
                 </div>
